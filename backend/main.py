e40f390aa68a64c6bbef693afcb267223ce1899c
NP <133921604+Franmarr@users.noreply.github.com>
2024-07-26 10:52:08 -0300

    se agrego CORS

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,25 @@
-import logging
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI
 from appSoap import conversor
+from fastapi.middleware.cors import CORSMiddleware
 
-logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/soap/")
 async def crear_planilla(number: dict):

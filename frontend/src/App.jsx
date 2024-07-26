bcb4792f9fc14c31366f72ae6cf543eb3f18a829
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-07-26 12:36:15 -0300

    Fix: Se creo el provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 // import logo from "./logo.svg";
+import { Provider } from "react-redux";
 import "./App.css";
 import Clima from "./components/Clima";
+import { store } from "./store/store";
 
 //Esto seria como el main
 function App() {
     return (
-        <div className="App">
-            <Clima />
-        </div>
+        <Provider store={store}>
+            <div className="App">
+                <Clima />
+            </div>
+        </Provider>
     );
 }
 

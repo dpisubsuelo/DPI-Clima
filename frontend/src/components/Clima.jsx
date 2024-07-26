f1b9c4e464c99bcda790ae7200b596f6ffa48a12
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-07-26 12:37:38 -0300

    Fix: Se corrigió el manejador de estados

--- frontend/src/components/Clima.jsx
+++ frontend/src/components/Clima.jsx
@@ -1,31 +1,50 @@
 import { useDispatch, useSelector } from "react-redux";
-import { usePostClimaMutation } from '../store/apiSlice';
+import { usePostClimaMutation, useGetClimaQuery } from "../store/apiSlice";
+import { useState } from "react";
 
 const Clima = () => {
+    const [formulario, setFormulario] = useState("");
+    const [grados, setGrados] = useState("0");
+    const [apiEnviarGrados] = usePostClimaMutation();
+    const dispatch = useDispatch();
 
-  const [apiEnviarGrados] = usePostClimaMutation();
-  const dispatch = useDispatch()
-  
     const manejarCambio = (e) => {
-    const { name, value } = e.target;
-      dispatch(apiEnviarGrados({ [name]: value }));
-    }
-  
+        const { value } = e.target;
+        setFormulario(value);
+        // console.log(formulario);
+        // dispatch(apiEnviarGrados({ [name]: value }));
+    };
+    const { data, error, isError, isLoading } = useGetClimaQuery(grados);
+    console.log(data);
+
     const enviar = (e) => {
-    e.preventDefault();
-    try {
-      dispatch(apiAddOrdenTrabajo(formulario));
-    } catch {
-      console.error();
+        console.log(formulario);
+        setGrados(formulario);
+        // e.preventDefault();
+        // try {
+        //     dispatch(apiEnviarGrados(formulario));
+        // } catch {
+        //     console.error();
+        // }
     };
 
-  }
-  
-  
-  return(
-  <>
-  <input type="text" name="grados" onChange={manejarCambio} />
-  
-  </>
-)}
-export default Clima
+    if (isLoading) return <div>Cargando</div>;
+    // else if (data) return <div>Con datos</div>;
+    else if (isError) return <div>Error: {error.message}</div>;
+
+    return (
+        <>
+            <input
+                type="text"
+                name="grados"
+                value={formulario.grados}
+                onChange={manejarCambio}
+            />
+            <button onClick={enviar}>Enviar</button>
+            {isLoading && <p>Cargando...</p>}
+            {error && <p>Error</p>}
+            {data && grados !== "0" && <p>{data}</p>}
+        </>
+    );
+};
+export default Clima;
